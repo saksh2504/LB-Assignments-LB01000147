7f0803699f6df47f8cadca4a464b88a0f4a49620
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-05-22 15:23:56 +0530

    Update program1.c

--- Assignment3/program1.c
+++ Assignment3/program1.c
@@ -1,1 +1,28 @@
+//program which accepts one number from user and print that number of even numbers on screen
+#include<stdio.h>
+void printEven(int iNo)
+{
+    int iCnt = 0;
+    if(iNo<=0)
+    {
+        return;
+    }
+
+    for(iCnt=1;iCnt<=iNo;iCnt++)
+    {
+        printf("%d",iCnt*2);
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    printEven(iValue);
+
+    return 0;
+}
 
