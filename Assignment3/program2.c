12500f678e555579e9b4e035f0eb27fc5cf2a7e7
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-05-22 15:25:04 +0530

    Add files via upload

---
+++ Assignment3/program2.c
@@ -0,0 +1,30 @@
+//program which accepts number from user and print factors of that number
+#include<stdio.h>
+void DisplayFactor(int iNo)
+{
+    int i = 0;
+
+    if(iNo <= 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(i=1; i<=iNo/2; i++)
+    {
+        if(iNo%i==0)
+        {
+            printf("%d",i);
+        }
+    }
+    
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    DisplayFactor(iValue);
+    return 0;
+}
+
---
+++ Assignment3/program3.c
@@ -0,0 +1,27 @@
+////program which accepts number from user and print Even factors of that number
+#include<stdio.h>
+void DisplayEvenFactor(int iNo)
+{
+    int i = 0;
+    if(iNo<=0)
+    {
+        iNo = -iNo;
+    }
+    for(i=1;i<=iNo/2;i++)
+    {
+        if((iNo%i==0)&&(i%2==0))
+        {
+            printf("%d",i);
+        }
+
+    }
+
+}
+int main()
+{
+    int iValue=0;
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+    DisplayEvenFactor(iValue);
+    return 0;
+}
