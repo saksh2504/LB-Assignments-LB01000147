d864724361bd66881224ee59e074660aff89c483
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-05-25 23:40:50 +0530

    Implement multiplication factors calculation
    The program calculates and displays the multiplication factors of a user-provided number.

--- Assignment4/program1.c
+++ Assignment4/program1.c
@@ -1,1 +1,36 @@
+//program which accept number from user and dsiplay its multiplication factors
+#include<stdio.h>
+int MultiFact(int iNo)
+{
+    int iCnt = 0;
+    int iMulti = 1;
 
+    for(iCnt=1; iCnt<=iNo; iCnt++)
+    {
+        if((iNo % iCnt)==0)
+        {
+            printf("%d",iCnt);
+            if(iCnt != iNo)
+            {
+                printf("*");
+            }
+         iMulti = iMulti * iCnt;
+        }
+          
+    }
+    return iMulti;
+}
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    iRet = MultiFact(iValue);
+
+    printf("=%d",iRet);
+
+    return 0;
+}
