9c76943926b044f47696358c1afa3f2d93364ba0
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-05-27 22:33:29 +0530

    Add program to display pattern based on input number
    Implement a program to display a pattern based on user input.

---
+++ Assignment7/program1.c
@@ -0,0 +1,31 @@
+//write program which Accept number from user and display below pattern
+#include<stdio.h>
+void Display(int iNo)
+{
+    int iCnt = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    for(iCnt = 1;iCnt <= (iNo*2);iCnt++)
+    {
+        if(iCnt <= iNo)
+        {
+             printf("*\t");
+        }
+        else
+        {
+            printf("#\t");
+        }  
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    return 0;
+}
